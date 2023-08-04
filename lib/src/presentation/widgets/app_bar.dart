17f50a91787d25d7d023fddb1c643c4f3cad2fc9
ShySpyDie <92712724+ShySpyDie@users.noreply.github.com>
2023-08-04 16:35:08 +0300

    feat: add widget AppBar

---
+++ lib/src/presentation/widgets/app_bar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppBarCard extends StatelessWidget {
+  const AppBarCard();
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar();
+  }
+}
