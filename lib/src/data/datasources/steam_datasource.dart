d30861ec5ad87eb8d0b07a14ba09de32507f9790
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2023-08-04 17:57:17 +0300

    feat: add getModelById impl

--- lib/src/data/datasources/steam_datasource.dart
+++ lib/src/data/datasources/steam_datasource.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import '../models/steam_profile_model.dart';
+
+import 'package:http/http.dart' as http;
 
 abstract class SteamDatasource {
   Future<ProfileModel> getModelByID(String id);
@@ -6,8 +10,21 @@ abstract class SteamDatasource {
 
 class SteamDatasourceImpl implements SteamDatasource {
   @override
-  Future<ProfileModel> getModelByID(String id) {
-    // TODO: implement getModelByID
-    throw UnimplementedError();
+  Future<ProfileModel> getModelByID(String id) async {
+    var uri = Uri.https(
+      'playerdb.co',
+      '/api/player/steam/$id',
+    );
+
+    final http.Response response = await http.get(uri);
+    final jsonResult = jsonDecode(response.body)['data']['player'];
+
+    ProfileModel profileModel = ProfileModel(
+      id: jsonResult['id'],
+      userName: jsonResult['username'],
+      avatar: jsonResult['avatar'],
+    );
+
+    return profileModel;
   }
 }
