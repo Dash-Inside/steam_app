16dafe89a47455107d500c1d7b3ac2937fb33875
ShySpyDie <92712724+ShySpyDie@users.noreply.github.com>
2023-08-04 17:35:02 +0300

    feat: add appbar logic

--- lib/src/presentation/widgets/app_bar.dart
+++ lib/src/presentation/widgets/app_bar.dart
@@ -1,10 +1,50 @@
 import 'package:flutter/material.dart';
 
-class AppBarCard extends StatelessWidget {
-  const AppBarCard();
+class MyAppBar extends StatelessWidget {
+  String discriptionAppBar;
+  bool showLeading;
+
+  MyAppBar({
+    super.key,
+    required this.discriptionAppBar,
+    required this.showLeading,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return AppBar();
+    // showLeading??
+    return Builder(builder: (context) {
+      if (showLeading) {
+        return AppBar(
+          backgroundColor: Theme.of(context).colorScheme.secondary,
+          title: Text(
+            discriptionAppBar,
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.tertiary,
+              fontSize: 24.0,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          leading: IconButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            icon: const Icon(Icons.arrow_back_ios_new_rounded),
+          ),
+        );
+      } else {
+        return AppBar(
+          backgroundColor: Theme.of(context).colorScheme.secondary,
+          title: Text(
+            discriptionAppBar,
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.tertiary,
+              fontSize: 24.0,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        );
+      }
+    });
   }
 }
