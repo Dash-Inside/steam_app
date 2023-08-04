0d70f8702ad1d6ca7303206d4629fabc15f7b0ee
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2023-08-04 18:01:51 +0300

    feat: add getSteamProfile impl

--- lib/src/data/repositories/steam_repository_impl.dart
+++ lib/src/data/repositories/steam_repository_impl.dart
@@ -2,6 +2,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:steam/core/failure/failuter.dart';
 import 'package:steam/src/domain/entities/steam_profile.dart';
+import 'package:steam/src/domain/failure/incorrect_id_failure.dart';
 import 'package:steam/src/domain/repositories/steam_repository.dart';
 import '../datasources/steam_datasource.dart';
 
@@ -13,8 +14,11 @@ class SteamRepositoryImpl implements SteamRepository {
   });
 
   @override
-  Future<Either<Failure, SteamProfile>> getSteamProfile({required String id}) {
-    // TODO: implement getSteamProfile
-    throw UnimplementedError();
+  Future<Either<Failure, SteamProfile>> getSteamProfile({required String id}) async {
+    try {
+      return Right(await steamDatasource.getModelByID(id));
+    } catch (_) {
+      return const Left(IncorrectIdFailure());
+    }
   }
 }
